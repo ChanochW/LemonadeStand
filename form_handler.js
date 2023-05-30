4431e96e40c1ac23cc9a1296799709c22a8ec81a
ChanochW <cwinogra@student.touro.edu>
2023-05-29 23:37:19 -0400

    Standard js code to handle the form being filled.

--- form_handler.js
+++ form_handler.js
@@ -2,10 +2,13 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 
+const email = process.env.FoodOrderBookEmail
+const pass = process.env.FoodOrderBookAppPass
+
 http.createServer(function(req, res){
     var q = url.parse(req.url, true);
     res.writeHead(200, {'Content-type': 'text/html'});
-    fs.readFile("./form.html", function(err, data){
+    fs.readFile("./order_form.html", function(err, data){
         if(err){
             res.writeHead(200, {'Content-type': 'text/html'});
             return res.end("404 error not found.")
@@ -13,11 +16,11 @@ http.createServer(function(req, res){
         res.write(data);
     });
 
-    if(q.pathname === "/form"){
+    if(q.pathname === "/order_form"){
         var query = q.query;
         console.log(query);
         console.log(query.fname);
-        console.log("You did a get from form.");
+        console.log("You did a get from order_form.");
         console.log(q.pathname);
         res.write("Name: " + query.fname + " " + query.lname );
         res.write("<br>Message: " + query.message);
@@ -35,14 +38,14 @@ function sendAnEmail(fname, lname, message, emailAdd){
     var transport = nodemailer.createTransport({
         service: 'gmail',
         auth: {
-            user: 'YOUREMAIL@gmail.com',
-            pass: 'YOURPASSWORD'
+            user: email,
+            pass: pass
         }
     });
 
     var mailOptions = {
-        from: 'YOUREMAIL@gmail.com',
-        to: 'YOUREMAIL@gmail.com',
+        from: email,
+        to: email,
         subject: 'Message received from form submission',
         html: '<h3>From: ' + fname + ' ' + lname + '</h3>'+
             '<h3>Email Address: ' + emailAdd + '</h3>' +
